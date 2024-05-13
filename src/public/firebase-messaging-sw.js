f70af6c61588d1ddafdef87b70383f1ae784fcd2
Cristóbal Berríos <crisberrios@uc.cl>
2024-05-12 21:10:35 -0400

    working push notifications

--- src/public/firebase-messaging-sw.js
+++ src/public/firebase-messaging-sw.js
@@ -12,13 +12,3 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
-
-// onBackgroundMessage(messaging, (payload) => {
-//   console.log("[firebase-messaging-sw.js] Received background message ", payload);
-//   const notificationTitle = "Background Message Title";
-//   const notificationOptions = {
-//     body: "Background Message body.",
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
